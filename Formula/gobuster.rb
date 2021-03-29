831ff56fcca909fd0631a85fc9018f558a5a4a6f
Bo Anderson <mail@boanderson.me>
2021-03-30 00:11:31 +0100

    gobuster: use std_go_args(ldflags:)

--- Formula/gobuster.rb
+++ Formula/gobuster.rb
@@ -17,7 +17,7 @@ class Gobuster < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-s -w", "-trimpath", "-o", bin/"gobuster"
+    system "go", "build", *std_go_args(ldflags: "-s -w")
     prefix.install_metafiles
   end
 
