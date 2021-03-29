8b0ccad12f39b6f6c51d558fad31c3a980ddbadc
Bo Anderson <mail@boanderson.me>
2021-03-30 00:11:38 +0100

    influxdb@1: use std_go_args(ldflags:)

--- Formula/influxdb@1.rb
+++ Formula/influxdb@1.rb
@@ -25,7 +25,7 @@ class InfluxdbAT1 < Formula
     ldflags = "-s -w -X main.version=#{version}"
 
     %w[influxd influx influx_tsm influx_stress influx_inspect].each do |f|
-      system "go", "build", "-ldflags", ldflags, *std_go_args, "-o", bin/f, "./cmd/#{f}"
+      system "go", "build", *std_go_args(ldflags: ldflags), "-o", bin/f, "./cmd/#{f}"
     end
 
     etc.install "etc/config.sample.toml" => "influxdb.conf"
