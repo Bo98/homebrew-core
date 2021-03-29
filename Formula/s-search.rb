cbb0b7ce6cee7a776a86ddf819b110a9e95e7a6c
Bo Anderson <mail@boanderson.me>
2021-03-30 00:11:57 +0100

    s-search: use std_go_args(ldflags:)

--- Formula/s-search.rb
+++ Formula/s-search.rb
@@ -16,7 +16,7 @@ class SSearch < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "-ldflags", "-s -w"
+    system "go", "build", *std_go_args(ldflags: "-s -w")
     mv bin/"s-search", bin/"s"
 
     bash_completion.install "autocomplete/s-completion.bash"
