17cffa368be2db3807df5f08adc45c06eab6c357
Bo Anderson <mail@boanderson.me>
2021-03-30 00:11:29 +0100

    go-jsonnet: use std_go_args(ldflags:)

--- Formula/go-jsonnet.rb
+++ Formula/go-jsonnet.rb
@@ -19,10 +19,10 @@ class GoJsonnet < Formula
   conflicts_with "jsonnet", because: "both install binaries with the same name"
 
   def install
-    system "go", "build", "-o", bin/"jsonnet", "./cmd/jsonnet"
-    system "go", "build", "-o", bin/"jsonnetfmt", "./cmd/jsonnetfmt"
-    system "go", "build", "-o", bin/"jsonnet-lint", "./cmd/jsonnet-lint"
-    system "go", "build", "-o", bin/"jsonnet-deps", "./cmd/jsonnet-deps"
+    system "go", "build", *std_go_args, "-o", bin/"jsonnet", "./cmd/jsonnet"
+    system "go", "build", *std_go_args, "-o", bin/"jsonnetfmt", "./cmd/jsonnetfmt"
+    system "go", "build", *std_go_args, "-o", bin/"jsonnet-lint", "./cmd/jsonnet-lint"
+    system "go", "build", *std_go_args, "-o", bin/"jsonnet-deps", "./cmd/jsonnet-deps"
   end
 
   test do
