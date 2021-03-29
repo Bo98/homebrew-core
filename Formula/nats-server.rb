f9a9a1b548a089e1fd43cf0c2d50bbed5a28350e
Bo Anderson <mail@boanderson.me>
2021-03-30 00:11:50 +0100

    nats-server: use std_go_args(ldflags:)

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -16,7 +16,7 @@ class NatsServer < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-s -w", *std_go_args
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   plist_options manual: "nats-server"
