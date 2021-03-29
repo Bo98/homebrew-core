5d53909c080e6caf019f100b40f5d328b4853419
Bo Anderson <mail@boanderson.me>
2021-03-30 00:11:22 +0100

    fcct: use std_go_args(ldflags:)

--- Formula/fcct.rb
+++ Formula/fcct.rb
@@ -22,8 +22,7 @@ class Fcct < Formula
 
   def install
     system "go", "build", "-mod=vendor",
-      "-ldflags", "-w -X github.com/coreos/fcct/internal/version.Raw=#{version}",
-      *std_go_args, "internal/main.go"
+      *std_go_args(ldflags: "-w -X github.com/coreos/fcct/internal/version.Raw=#{version}"), "internal/main.go"
 
     prefix.install_metafiles
   end
