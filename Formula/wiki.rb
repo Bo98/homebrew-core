c07976b54619cee898caa87a8d019f891f3c921f
Bo Anderson <mail@boanderson.me>
2021-03-30 00:12:13 +0100

    wiki: use std_go_args(ldflags:)

--- Formula/wiki.rb
+++ Formula/wiki.rb
@@ -16,7 +16,7 @@ class Wiki < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/"wiki", "cmd/wiki/main.go"
+    system "go", "build", *std_go_args, "cmd/wiki/main.go"
   end
 
   test do
