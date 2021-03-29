8e0df43befec8aaaf44082f5a8f1c300828ec151
Bo Anderson <mail@boanderson.me>
2021-03-30 00:11:11 +0100

    cql: use std_go_args(ldflags:)

--- Formula/cql.rb
+++ Formula/cql.rb
@@ -22,8 +22,7 @@ class Cql < Formula
     ldflags = "-s -w -X main.version=v#{version} " \
       "-X github.com/CovenantSQL/CovenantSQL/conf.RoleTag=C " \
       "-X github.com/CovenantSQL/CovenantSQL/utils/log.SimpleLog=Y"
-    system "go", "build", *std_go_args, "-tags", "sqlite_omit_load_extension",
-      "-ldflags", ldflags, "./cmd/cql"
+    system "go", "build", *std_go_args(ldflags: ldflags), "-tags", "sqlite_omit_load_extension", "./cmd/cql"
 
     bash_completion.install "bin/completion/cql-completion.bash"
     zsh_completion.install "bin/completion/_cql"
