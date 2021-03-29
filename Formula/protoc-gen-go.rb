7a8fc3fb1d71f401ed4c1d55e766a948f84fcbbf
Bo Anderson <mail@boanderson.me>
2021-03-30 00:11:54 +0100

    protoc-gen-go: use std_go_args(ldflags:)

--- Formula/protoc-gen-go.rb
+++ Formula/protoc-gen-go.rb
@@ -17,7 +17,7 @@ class ProtocGenGo < Formula
   depends_on "protobuf"
 
   def install
-    system "go", "build", *std_go_args, "-ldflags", "-s -w", "./protoc-gen-go"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./protoc-gen-go"
     prefix.install_metafiles
   end
 
