d9b90e76f167ee56d47e599e673f40c3dba09b79
Bo Anderson <mail@boanderson.me>
2021-03-30 00:11:10 +0100

    clipper: use std_go_args(ldflags:)

--- Formula/clipper.rb
+++ Formula/clipper.rb
@@ -17,7 +17,7 @@ class Clipper < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-s -w", "-trimpath", "-o", bin/"clipper", "clipper.go"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "clipper.go"
   end
 
   plist_options manual: "clipper"
