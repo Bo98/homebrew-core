eba6195942fe5fd3823777366d752e6e7808b3c8
Bo Anderson <mail@boanderson.me>
2021-03-30 00:12:12 +0100

    wego: use std_go_args(ldflags:)

--- Formula/wego.rb
+++ Formula/wego.rb
@@ -43,7 +43,7 @@ class Wego < Formula
     (buildpath/"src/github.com/schachmat").mkpath
     ln_sf buildpath, buildpath/"src/github.com/schachmat/wego"
     Language::Go.stage_deps resources, buildpath/"src"
-    system "go", "build", "-o", bin/"wego"
+    system "go", "build", *std_go_args
   end
 
   test do
