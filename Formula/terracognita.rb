5dbb2f99b2da86e65cbfc47e6aa09e61cc5504a4
Bo Anderson <mail@boanderson.me>
2021-03-30 00:12:03 +0100

    terracognita: use std_go_args(ldflags:)

--- Formula/terracognita.rb
+++ Formula/terracognita.rb
@@ -17,7 +17,7 @@ class Terracognita < Formula
 
   def install
     ldflags = "-X github.com/cycloidio/terracognita/cmd.Version=v#{version}"
-    system "go", "build", *std_go_args, "-ldflags", ldflags
+    system "go", "build", *std_go_args(ldflags: ldflags)
   end
 
   test do
