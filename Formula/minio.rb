e249874c65c6a79310cec9c73aa64d16d8a2138e
Bo Anderson <mail@boanderson.me>
2021-03-30 00:11:49 +0100

    minio: use std_go_args(ldflags:)

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -33,7 +33,7 @@ class Minio < Formula
       version = release.gsub(/RELEASE\./, "").chomp.gsub(/T(\d+)-(\d+)-(\d+)Z/, 'T\1:\2:\3Z')
       proj = "github.com/minio/minio"
 
-      system "go", "build", *std_go_args, "-ldflags", <<~EOS
+      system "go", "build", *std_go_args(ldflags: <<~EOS)
         -X #{proj}/cmd.Version=#{version}
         -X #{proj}/cmd.ReleaseTag=#{release}
         -X #{proj}/cmd.CommitID=#{Utils.git_head}
